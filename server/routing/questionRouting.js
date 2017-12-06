1ed479adfce70c15a3dd0e553492590f625ebe47
Shadowphoenix <github@shadowphoenix.nl>
2017-12-06 11:36:27 +0100

    Filled in some queries for question and goal

--- server/routing/questionRouting.js
+++ server/routing/questionRouting.js
@@ -1,13 +1,44 @@
 const express = require('express');
 const Question = require('../models/Question');
+const repo = require('../repositories/questionRepository');
+const questionRepo = new repo();
 
 let router = express.Router();
 
 router.get('/', function(req,res) {
     let text = `Hello User,
-        this part of the API will be filled in soon`;
+        getById = question/byId/id
+        filterOnQuestion = question/filter/any letter you want to search on
+        getByGoal = question/byGoal/goal_id
+        create = question/create`;
     res.send(text);
+});
+
+router.get('/byId/:identifier', function(req,res) {
+    questionRepo.getQuestionById(req.params.identifier)
+        .then(question => {
+            res.send(question);
+        });
+});
+
+router.get('/filter/:identifier', function(req,res) {
+    questionRepo.getQuestionByText(req.params.identifier)
+        .then(question => {
+            res.send(question);
+        });
+});
+
+router.get('/byGoal/:identifier', function(req,res) {
+    questionRepo.getQuestionsByGoal(req.params.identifier)
+        .then(questions => {
+            res.send(questions);
+        });
+});
+
+router.post('/create', function(req,res) {
+    let data = req.body;
+    questionRepo.create(data.question, data.questionType, data.goal_id);
+    res.send("Done");
 })
-router.get('/')
 
 module.exports = router;
