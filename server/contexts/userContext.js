b0aeca2be7490ebefe465c6e0431cc74ed3e716a
Shadowphoenix <github@shadowphoenix.nl>
2017-12-13 09:02:11 +0100

    minor changes

--- server/contexts/userContext.js
+++ server/contexts/userContext.js
@@ -5,12 +5,12 @@ class userContext {
     getUserByEmail(email){
         return User.query()
         .where('email', email)
-        .eager('[sendFeedback,receivedFeedback]');
+        .eager('[sendFeedback,receivedFeedback, leadingTeams, memberOfTeams, goals]');
     }
     getUserById(id){
         return User.query()
         .where('id', id)
-        .eager('[sendFeedback,receivedFeedback]');
+        .eager('[sendFeedback,receivedFeedback, leadingTeams, memberOfTeams, goals]');
     }
     getNameById(id){
         return User.query()
@@ -34,10 +34,12 @@ class userContext {
         return User.query()
         .where(User.raw('lower("firstName")'),'LIKE', search)
         .orWhere(User.raw('lower("lastName")'), 'LIKE', search)
-        .eager('[sendFeedback,receivedFeedback]');
+        .eager('[sendFeedback,receivedFeedback, leadingTeams, memberOfTeams, goals]');
     }
     login(email,password){
-        return User.query().where('email',email);
+        return User.query()
+            .where('email',email)
+            .eager('[sendFeedback, receivedFeedback, leadingTeams, memberOfTeams, goals]');
     }
 }
 
