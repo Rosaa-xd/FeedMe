33f68aae80f8f6f7bcb9c067a45699b26796cc45
Shadowphoenix <github@shadowphoenix.nl>
2018-01-15 11:04:13 +0100

    minor goldcard fix

--- server/routing/goldCardRouting.js
+++ server/routing/goldCardRouting.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const User = require('../models/GoldCard');
 const repo = require('../repositories/goldCardRepository');
+const repoUser = require('../repositories/userRepository');
 const goldCardRepo = new repo();
+const userRepo = new repoUser();
 
 let router = express.Router();
 
 router.post('/create', function(req, res) {
-    goldCardRepo.checkGoldCard(req.body.user_id)
+    goldCardRepo.checkGoldCard(req.body.receiver_id)
         .then(function(goldCard) {
             if (goldCard.rowCount !== 0) {
                 res.send("This person already had the goldcard in the last 7 days");
             }
             else {
-                goldCardRepo.createGoldCard(req.body.user_id);
-                res.send("The goldcard has been given.");
+                goldCardRepo.createGoldCard(req.body.receiver_id);
+                userRepo.giveGoldCard(req.body.receiver_id);
+                userRepo.getUserById(req.body.sender_id)
+                    .then(user => {
+                        res.send(user);
+                    });
             }
         });
 })
